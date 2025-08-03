c66483012c1b5a8f290f3a4563e0a6641ae13cc0
saddiq musa <saddiq0musa@gmail.com>
2025-08-03 20:20:07 +0300

    feat: configure webpack to resolve component aliases

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,19 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
-	/* config options here */
 	eslint: {
 		ignoreDuringBuilds: true,
+	},
+	webpack: (config) => {
+		config.resolve = {
+			...(config.resolve || {}),
+			alias: {
+				...(config.resolve?.alias || {}),
+				"@components": path.resolve(__dirname, "src/components"),
+			},
+		};
+		return config;
 	},
 };
 
